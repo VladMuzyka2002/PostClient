51bd20a81636b07fd5227e4d6ed80a2b22c177f1
Vlad Muzyka <angryvlad@gmail.com>
2024-06-14 12:05:57 -0400

    Complete Client

---
+++ src/pages/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Test() {
+  return (
+    <div>
+      <h1>Welcome to the Test Page</h1>
+      <p>This is a simple page that prints something on the screen.</p>
+    </div>
+  );
+}
+
+export default Test;
